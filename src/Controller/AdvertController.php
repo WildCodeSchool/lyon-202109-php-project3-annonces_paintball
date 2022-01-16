f13aff6d76171123bff8b4eca4539acaf5e7cfb6
Sandrine Boutier <sandrineboutier@gmail.com>
2022-01-16 16:27:19 +0100

    trying to solve manifest.json does not exist exception

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -7,6 +7,8 @@ use phpDocumentor\Reflection\Types\Void_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * @Route("/advert", name="advert_")
@@ -39,5 +41,19 @@ class AdvertController extends AbstractController
     public function advertSearch(): Response
     {
         return $this->render('advert/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/contactseller", name="contactseller")
+     */
+    public function sendEmail(MailerInterface $mailer): void
+    {
+        $email = (new Email())
+            ->from('noreply@annoncespaintball.com')
+            ->to('sender@demo.com')
+            ->subject('Une personne est intéressée par votre annonce sur Annonces Paintball')
+            ->html('<p>Une personne est intéressée par votre annonce sur Annonces Paintball</p>');
+
+        $mailer->send($email);
     }
 }
