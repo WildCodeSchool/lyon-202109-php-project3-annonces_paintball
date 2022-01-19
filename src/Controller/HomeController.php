228636b7f9e7137b346576e9da7f07e2f7fef39e
Falilou1 <falou40@gmail.com>
2022-01-19 13:08:48 +0100

    homeview

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 /**
  * @Route("/home", name="home_")
  */
@@ -18,6 +17,5 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
- 
     }
 }
